35dfefe1e9591067fe74c557c321c01ef7334413
worace <horace.d.williams@gmail.com>
2015-04-08 10:56:28 -0600

    trying another config approach

--- lib/turing_auth.rb
+++ lib/turing_auth.rb
@@ -2,14 +2,29 @@ require "turing_auth/version"
 require "turing_auth/teams"
 require "turing_auth/user"
 require "turing_auth/current_user"
-require "turing_auth/controller_methods"
 
 module TuringAuth
+  def self.client_id=(client_id)
+    @@client_id = client_id
+  end
+
+  def self.client_id
+    @@client_id
+  end
+
+  def self.client_secret=(client_secret)
+    @@client_secret = client_secret
+  end
+
+  def self.client_secret
+    @@client_secret
+  end
+
   def self.init!
     if defined?(Rails) && defined?(OmniAuth::Builder)
       Rails.application.config.middleware.use OmniAuth::Builder do
         provider :developer unless Rails.env.production?
-        provider :github, ENV['NAMER_GITHUB_ID'], ENV['NAMER_GITHUB_SECRET'], scope: "user"
+        provider :github, TuringAuth.client_id, TuringAuth.client_secret, scope: "user"
       end
     else
       raise "TuringAuth currently only supports Rails apps; make sure you're using it in a Rails app."
