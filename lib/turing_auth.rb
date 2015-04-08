6274569581d13ec50b27b4187fbc7395acfcd361
worace <horace.d.williams@gmail.com>
2015-04-08 10:50:38 -0600

    import Omniauth init logic from app

--- lib/turing_auth.rb
+++ lib/turing_auth.rb
@@ -5,4 +5,14 @@ require "turing_auth/current_user"
 require "turing_auth/controller_methods"
 
 module TuringAuth
+  def self.init!
+    if defined?(Rails) && defined?(OmniAuth::Builder)
+      Rails.application.config.middleware.use OmniAuth::Builder do
+        provider :developer unless Rails.env.production?
+        provider :github, ENV['NAMER_GITHUB_ID'], ENV['NAMER_GITHUB_SECRET'], scope: "user"
+      end
+    else
+      raise "TuringAuth currently only supports Rails apps; make sure you're using it in a Rails app."
+    end
+  end
 end
