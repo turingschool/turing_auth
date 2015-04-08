fa433c39a36a6be68fe80bd3326aaa7532d48c43
worace <horace.d.williams@gmail.com>
2015-04-08 10:18:27 -0600

    migrate current user method to create TuringAuth::User objects from session data

---
+++ lib/turing_auth/current_user.rb
@@ -0,0 +1,14 @@
+# include this module into a rails controller
+# to add current_user functionality.
+module TuringAuth
+  module CurrentUser
+    def current_user
+      return nil unless session[:current_user]
+      @current_user ||= TuringAuth::User.new(session[:current_user])
+    end
+
+    def self.included(klass)
+      klass.send(:helper_method, :current_user) if klass.respond_to?(:helper_method)
+    end
+  end
+end
