d88db586146cc951dcea477c29019c93c33e80df
worace <horace.d.williams@gmail.com>
2015-04-08 10:19:31 -0600

    migrate initial TuringAuth::User class for creating github-based users out of session data

---
+++ lib/turing_auth/user.rb
@@ -0,0 +1,27 @@
+module TuringAuth
+  class User
+    attr_reader :github_id, :github_name, :email, :github_token, :authed_at
+
+    def initialize(attributes)
+      attributes.symbolize_keys!
+      @github_id = attributes[:github_id].to_i
+      @github_name = attributes[:github_name]
+      @email = attributes[:email]
+      @github_token = attributes[:github_token]
+      @authed_at = attributes[:authed_at] || Time.now
+    end
+
+    def as_json(options={})
+      {github_id: github_id, github_name: github_name,
+       email: email, github_token: github_token, authed_at: authed_at}
+    end
+
+    def to_json
+      as_json.to_json
+    end
+
+    def inspect
+      "Turing GH User: #{github_name}, gh id: #{github_id}, email #{email}, token: #{github_token}"
+    end
+  end
+end
