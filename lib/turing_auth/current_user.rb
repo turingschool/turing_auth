236032ab6d902f070b0fd7d6dade7be13f45b0b2
worace <horace.d.williams@gmail.com>
2015-04-14 20:41:25 -0600

    add logged_in? controller method

--- lib/turing_auth/current_user.rb
+++ lib/turing_auth/current_user.rb
@@ -7,8 +7,13 @@ module TuringAuth
       @current_user ||= TuringAuth::User.new(session[:current_user])
     end
 
+    def logged_in?
+      !!current_user
+    end
+
     def self.included(klass)
       klass.send(:helper_method, :current_user) if klass.respond_to?(:helper_method)
+      klass.send(:helper_method, :logged_in?) if klass.respond_to?(:helper_method)
     end
   end
 end
